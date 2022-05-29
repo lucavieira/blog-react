a6b4cec1b90d2855978e546602c1e7da00365a8e
lucavieira <lukasveras14@gmail.com>
2022-05-28 21:51:15 -0300

    Botão favoritar/gostar adicionado.

---
+++ src/assets/styles/Heart.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Heart = styled.img`
+    position: absolute;
+    top: 12px;
+    right: 12px;
+    cursor: pointer;
+`
+
+export default Heart
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,29 @@ import ContainerCard from "../assets/styles/ContainerCard"
 import Date from "../assets/styles/Date"
 import Title from "../assets/styles/Title"
 import Content from "../assets/styles/Content"
+import Heart from "../assets/styles/Heart"
+
+import heart_empty from "../assets/images/heart_empty.png"
+import heart_full from "../assets/images/heart_full.png"
 
 const Card = ({date, title, content}) => {
+
+    const liked = (elemento) => {
+        if(elemento.target.alt == "Coração vazio") {
+            elemento.target.attributes[0].value = heart_full
+            elemento.target.alt = "Coração cheio"
+        } else {
+            elemento.target.attributes[0].value = heart_empty
+            elemento.target.alt = "Coração vazio"
+        }
+    }
+
     return (
         <ContainerCard>
             <Date>{date}</Date>
             <Title>{title}</Title>
             <Content>{content}</Content>
+            <Heart src={heart_empty} alt="Coração vazio" onClick={(elemento) => liked(elemento)}></Heart>
         </ContainerCard>
     )
 }
